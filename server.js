408dd15a0091c80472bc56cae0d2c736854f3abb
Anthea <anthea922@gmail.com>
2024-02-28 16:31:12 +0200

    Second commit

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+//const mongoose = require("mongoose");
+
+//import routes
+//const authRoute = require("./routes/auth");
 const app = express();
 
 app.use(express.json());
@@ -9,17 +13,29 @@ app.get("/", (req, res) => {
     res.send("Fullstack React Course Express Server");
 });
 
+
+
+//app.use("/api/auth", authRoute);
+
+/*mongoose
+    .connect(process.env.MONGO_URI).then(() => {
+        console.log("Connected to database");*/
+
 app.post("/name", (req, res) => {
     if (req.body.name) {
         return res.json({ name: req.body.name });
     } else {
         return res.status(400).json({ error: "No name provided" });
     }
-})
-
-
+    //})
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 });
+    /*.catch((error) => {
+console.log(error);
+});*/
+
+
 
